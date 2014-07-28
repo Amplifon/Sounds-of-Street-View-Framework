ec524833ef3b21fa99bccdadf1b26c739233191c
notepadwebdev <griffleriff@gmail.com>
2014-07-28 15:16:07 +0100

    initial commit of files

--- README.md
+++ README.md
@@ -1,4 +1,183 @@
-Sounds-of-Street-View-Framework
-===============================
+# SOSV Framework
 
-Sounds of Street View is a digital explorative sound experiment by Amplifon which gives users a 3-dimensional sound experience in a Street View environment. Utilising the Web Audio API platform, sounds are designated as though they were ordinary Google Maps markers, but instead of an image and information being assigned to the marker, a sound is instead.
+###A JavaScript library to add sound to a Google Street View experience.
+
+Watch the promotional video for an explanation and overview of what can be achieved 
+with the Sounds of Street View Framework.
+
+[![ScreenShot](https://raw.github.com/epiphanysearch/SoundsOfStreetViewFramework/src/img/vimeo-img.jpg)](https://vimeo.com/101599212)
+
+Explore three environments created by Amplifon on [the official Sounds of Street View website](http://www.amplifon.co.uk/sounds-of-street-view/).
+
+## Dependencies
+
+- [Google Maps API v3](https://developers.google.com/maps/documentation/javascript/basics)
+- [JQuery](http://jquery.com/)
+- [Howler - (epiphanysearch fork)](https://github.com/epiphanysearch/howler.js)
+
+## How to use it
+
+The following explanations are specifically written for those with only a 
+rudimentary understanding of HTML and JavaScript. If you are a developer, or 
+are confident that you have a decent understanding of these technologies, then 
+head straight over to the src folder, and look at the demo HTML file where everything should 
+be fairly self explanatory.
+
+### Include the dependencies
+
+The first thing to do is to include the 3 JavaScript dependencies within the head section 
+of your HTML page. Make sure that the version of Howler.js you include is the specific 
+epiphanysearch fork version as this contains extra code to support the 
+use of Web Audio API Low Pass Filters. Using this filter ensures that sounds become brighter 
+when positioned in front of the user. You can also find a minified version of the epiphanysearch 
+Howler.js in the js/vendor folder of this repo.
+
+```html
+    <!-- Load the Google Maps API -->
+    <script src="//maps.googleapis.com/maps/api/js?v=3.exp&sensor=false"></script>
+    
+    <!-- Load jQuery -->
+    <script src="//code.jquery.com/jquery-2.1.1.min.js"></script>
+
+    <!-- Load the epiphanysearch fork of Howler.js -->
+    <script src="js/vendor/howler.min.js"></script>
+```
+
+### Create an element on your page for the Google Street View instance
+
+Make sure when you create this HTML element that it has a unique ID, and that 
+the ID you provide it with matches the ID that you stipulate within your JSON data 
+file (see below)
+
+```html
+	<div id="pano"></div>
+```
+
+### Create your JSON data file
+
+The JSON file should contain all the data for your Sounds of Street View application, including 
+the start position of the map when the application loads, as well as the positioning and volume 
+levels for all of your sounds. The "id" property at the start of the file should exactly match the 
+unique ID that you gave the HTML element within your HTML (see above).
+
+This is the demo JSON file included within the src demo as part of this repo.
+
+```json
+	{
+	    "id": "pano",
+
+	    "lat": "43.950649",
+	    "lng": "4.806588",
+	    "heading": "60",
+	    "pitch": "1",
+	    
+	    "sounds": [
+	        {
+	            "name": "accordion",
+	            "lat": "43.950648973687",
+	            "lng": "4.806433016568121",
+	            "src": [
+	                "audio/accordian.mp3"
+	            ],
+	            "db": "80",
+	            "pause": "0"
+	        },
+	        {
+	            "name": "church-bell-2",
+	            "lat": "43.951337460699705",
+	            "lng": "4.8069440499275515",
+	            "src": [
+	                "audio/church-bell.mp3"
+	            ],
+	            "db": "100",
+	            "pause": "0"
+	        }
+
+	        ... add more sound objects here ...
+	    ]
+	}
+```
+#### JSON Properties
+
+Here is a more thorough breakdown of the properties included within the JSON data file;
+
+**id**  
+This should match the ID of your HTML element where the Google Street View will appear  
+
+**lat**  
+This is the latitude position value where your application will start upon load   
+
+**lng**  
+This is the longitude position value where your application will start upon load   
+
+**heading**  
+This is the heading position value where your application will start upon load (ie. which direction the user is facing)   
+
+**pitch**  
+This is the pitch position value where your application will start upon load (ie. how low or high the user is facing)   
+
+**sounds**  
+This is an array of sound objects (denoted by the square brackets) that will be positioned within your application. 
+The Framework does not impose any limits upon how many sounds you can add, however be aware that any computer viewing 
+your app *will* have limitations for how many current sounds it can track and play depending upon the processing 
+power of the machine. 
+
+#### Individual sound properties
+
+Each sound has the following properties;
+
+**name**  
+Although the user will not see this name, it can be useful when building your application as 
+the name of each sound object will appear on hover when you view your application in dev mode. 
+You can trigger dev mode by appending ?dev=true to the end of your application URL.
+
+**lat**  
+This is the latitude position where your sound will be positioned on your Street View map  
+
+**lng**  
+This is the longitude position where your sound will be positioned on your Street View map  
+
+**src**  
+The path to the mp3 file associated with the sound object
+
+**db**  
+The "loudness" of the sound, ie how far away it can be heard from. Although db suggests 
+that this is the decibel level for a sound it is not calculated at decibel levels. The db property 
+should be a value between 1 and 100, with 1 being very quiet and 100 being very loud.
+
+**pause**  
+The length of time to wait between each loop of the sound. This is a length of time in 
+milliseconds so a 1 second pause between each loop would have a pause value of 1000.
+
+### Debugging
+
+If you are unfamiliar with writing JSON then it can be frustrating as a simple comma in the wrong 
+place, or a missing quotation mark can break your application. You can use an online JSON linting 
+tool to find and remove any errors you may have in your data.
+
+- [JSONLint](http://jsonlint.com/)
+
+
+## License
+
+The MIT License
+
+Copyright (c) 2014 Epiphany Search
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
---
+++ src/js/sosv.js
@@ -0,0 +1,319 @@
+
+var getUrlVars = function() {
+    var vars = {};
+    var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(m, key, value) {
+        vars[key] = value;
+    });
+    return vars;
+};
+
+var devMode = getUrlVars().dev;
+
+var rad = function(x) {
+	return x * Math.PI / 180;
+};
+
+var Distance = function(p1, p2, metric){
+	var R = 6378137, 	// Earth’s mean radius in meter
+		dLat = rad(p2.lat() - p1.lat()),
+		dLong = rad(p2.lng() - p1.lng()),
+		a = Math.sin(dLat / 2) * Math.sin(dLat / 2) + Math.cos(rad(p1.lat())) * Math.cos(rad(p2.lat())) * Math.sin(dLong / 2) * Math.sin(dLong / 2),
+		c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a)),
+		d = R * c;		// d = the distance in meter
+
+		if (metric == 'miles') {		// convert to miles?
+			d = d * 0.000621371192;
+		}
+	
+	return d; 			
+}
+
+var Sound = function(json, panorama, _sosv){
+
+	var obj 	= this;
+	this.sosv 	= _sosv;
+	this.data 	= json;			// JSON data associated with this sound
+	this.map 	= panorama;		// Street view pano we are working with
+		
+	this.sound 	= null;			// Howler object
+	this.vol 	= 0;			// Volume of sound		
+
+	this.position 			= new google.maps.LatLng(this.data.lat, this.data.lng);
+	this.prevUserPosition 	= { lat: null, lng: null };
+	this.prevVolume 		= 0;
+
+	this.init = function(){
+
+		// Listen for user position change events
+		$(document.body)
+			.on('panoChanged', 		this.onUserMovement)
+			.on('positionChanged', 	this.onUserMovement)
+			.on('povChanged', 		this.onUserMovement);
+
+		this.createSound();
+		this.addSoundToMap();
+	};
+
+	this.createSound = function(){
+		
+		// Only use loop if pause = 0
+		var loop = (!parseFloat(obj.data.pause)) ? true : false;
+
+		obj.sound = new Howl({  
+			urls: obj.data.src, 
+			loop: loop,
+			onload: obj.onSoundLoaded,
+			onloaderror: obj.onSoundLoadError
+		});
+	};
+
+	this.onSoundLoaded = function(e){
+
+		$(document.body).trigger('soundLoaded', obj.data);
+	};
+
+	this.onSoundLoadError = function(e){
+
+		$(document.body).trigger('soundLoadError', obj.data);
+	};
+
+	this.addSoundToMap = function(){
+
+		obj.data.icon = (devMode) ? null : 'data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7';
+		obj.data.draggable = (devMode) ? true : false;
+		obj.sosv.addMarker(obj.data);
+
+		this.updatePan();
+	};
+
+	this.playSound = function(){
+
+		obj.sound.play();
+
+		// Manually loop the sound, interspesed with pauses, if we have a pause value for this object
+		if (parseFloat(obj.data.pause)) {
+			
+			obj.sound.on('end', function(){
+				
+				obj.sound.pause();
+
+				setTimeout(function(){
+					obj.sound.play();
+				}, parseInt(obj.data.pause));
+
+			});
+		}
+	};
+
+	this.stopSound = function(){
+		obj.sound.stop();
+	};
+
+	this.unloadSound = function(fadeSpeed){
+
+		obj.sound.fade(obj.vol, 0, fadeSpeed, function(){
+			obj.sound.unload();
+		});
+
+		// Sometimes the fade callback does not fire, so manually unload sound after fade length as failsafe
+		setTimeout(function(){
+			if (obj.sound) {
+				obj.sound.unload();
+			}
+		}, (fadeSpeed+50));
+	};
+
+	this.onUserMovement = function(e, pano){
+		
+		// Get current position data for the user
+		var lat 		= pano.getPosition().lat(),
+			lng 		= pano.getPosition().lng(),
+			heading 	= pano.getPov().heading;
+
+		obj.updatePan(lat, lng, heading);
+		obj.updateVolume(lat, lng, pano);
+	};
+
+	this.updatePan = function(lat, lng, heading){
+
+		var xDiff = obj.data.lat - lat,
+			yDiff = obj.data.lng - lng,
+			angle = Math.atan2(yDiff, xDiff) * (180/Math.PI);
+
+		// Add POV heading offset
+		angle -= heading;
+
+		// Convert angle to range between -180 and +180
+		if (angle < -180) 		angle += 360;
+		else if (angle > 180) 	angle -= 360;
+
+		// Calculate panPosition, as a range between -1 and +1
+		var panPosition = (angle/90);
+		if (Math.abs(panPosition) > 1) {
+			var x = Math.abs(panPosition) - 1;
+			panPosition = (panPosition > 0) ? 1 - x : -1 + x;
+		}
+
+		// Set the new pan poition
+		obj.sound.pos3d(panPosition, 1, 1);
+
+		// Apply lowpass filter *if* the sound is behind us (11,000hz = filter fully open)
+		var freq = 11000;
+		if (Math.abs(angle) > 90) {
+			// User's back is to the sound - progressively apply filter
+			freq -= (Math.abs(angle) - 90) * 55;
+		}
+		obj.sound.filter(freq);
+	};
+
+	this.updateVolume = function(lat, lng, pano){
+
+		if (lat !== obj.prevUserPosition.lat || lng !== obj.prevUserPosition.lng) {
+
+			// Calculate distance between user and sound
+			var distance = Distance(obj.position, pano.getPosition());
+			
+			// Calculate new volume based on distance
+			obj.vol = obj.calculateVolume(distance);
+
+			// Set new volume
+			obj.sound.fade(obj.prevVolume, obj.vol, 500);
+
+			// Cache the new volume / position for checking next time 
+			obj.prevVolume = obj.vol;
+			obj.prevUserPosition.lat = lat;
+			obj.prevUserPosition.lng = lng;
+		}
+	};
+
+	this.calculateVolume = function(distance){
+		// Calculate volume by using Inverse Square Law
+		obj.vol = 1 / (distance * distance);
+		// Multiply distance volume by amplitude of sound (apply ceiling max of 1)
+		obj.vol = Math.min((obj.vol * obj.data.db), 1);
+		return obj.vol;
+	};
+
+	this.init();
+}
+
+var SOSV = function(jsonPath){
+	
+	var self = this,
+		el,
+		panorama,
+		markers = [],
+		arrSounds = [],
+		soundCount = 0;
+
+	this.init = function(){
+		
+		// Test for presence of Web Audio API
+		if (!this.webApiTest) {
+			alert('Your browser does not support the Web Audio API!');
+			return;
+		}
+
+		$(document.body)
+			.on('soundLoaded', this.onSoundLoaded)
+			.on('soundLoadError', this.onSoundLoaded)
+			.on('changeLocation', this.onChangeLocation);
+		
+		// Load JSON data
+		$.getJSON(jsonPath, this.onJsonLoaded); 
+	};
+
+	this.webApiTest = function(){
+		var waAPI;
+		if (typeof AudioContext !== "undefined") {
+		    waAPI = new AudioContext();
+		} else if (typeof webkitAudioContext !== "undefined") {
+		    waAPI = new webkitAudioContext();
+		}
+		return (waAPI) ? true : false;
+	};
+
+	this.onJsonLoaded = function(data){
+		
+		soundCount = data.sounds.length;
+
+		self.createStreetView(data);
+		self.loadSounds(data);
+
+		// Manually trigger onSoundLoaded if there are no sounds in the json data
+		if (!soundCount) {
+			self.onSoundLoaded(null);
+		}
+	};
+
+	this.createStreetView = function(data){
+		
+		el = $('#'+data.id);
+		
+		panorama = new google.maps.StreetViewPanorama(document.getElementById(data.id), {
+				
+			position 			: new google.maps.LatLng(data.lat, data.lng),
+			pov: {
+			  	heading 		: Number(data.heading),
+			  	pitch 			: Number(data.pitch)
+			}
+		});
+		// add listeners
+		google.maps.event.addListener(panorama, 'pano_changed', 	this.onPanoChanged);
+		google.maps.event.addListener(panorama, 'position_changed', this.onPositionChanged);
+		google.maps.event.addListener(panorama, 'pov_changed', 		this.onPovChanged);
+	};
+
+	this.addMarker = function(data){
+		var marker = new google.maps.Marker({
+		    map 		: panorama,
+		    title 		: data.name,
+		    position 	: new google.maps.LatLng(data.lat, data.lng),
+		    draggable 	: data.draggable,
+		    icon 		: data.icon
+		});
+		markers.push(marker);
+	};
+
+	this.onPanoChanged = function(e){
+		el.trigger('panoChanged', panorama);
+	};
+
+	this.onPositionChanged = function(e){
+		el.trigger('positionChanged', panorama);
+	};
+
+	this.onPovChanged = function(e){
+		el.trigger('povChanged', panorama);
+	};
+
+	this.loadSounds = function(data){
+		
+		// Create all the sounds objects, and store in array
+		for (var i=0; i < data.sounds.length; i++) {
+			var sound = new Sound(data.sounds[i], panorama, self);
+			arrSounds.push(sound);
+		}
+	};
+
+	this.onSoundLoaded = function(e){
+
+		soundCount--;
+
+		// All sounds loaded?
+		if (soundCount <= 0) {
+			self.playSounds();
+		}
+	};
+
+	this.playSounds = function(){
+
+		// Start all sounds and trigger onUserMovement to set filters/pans etc
+		for (var i=0; i < arrSounds.length; i++) {
+			arrSounds[i].playSound();
+			arrSounds[i].onUserMovement(null, panorama);
+		}
+	};
+
+	this.init();
+}
